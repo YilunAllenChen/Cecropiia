ce009864a1ea00473bc0db984e6749beb2d97fc1
YilunAllenChen <allenchen@gatech.edu>
2020-03-03 08:25:10 -0500

    ip configed

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-mongoConfig = "mongodb://aiegSimulatormockApp:Lo0RSpu5-dF5xsd@192.168.30.215:27037/aiegSimulatormock"
+mongoConfig = "mongodb+srv://cecropia_admin:Slothbot@allenchen-h25gi.mongodb.net/test?retryWrites=true&w=majority"
 
