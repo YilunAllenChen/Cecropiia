c9b9f0ef99a21d5231dbedf8dfe7131c12262c6d
YilunAllenChen <allenchen@gatech.edu>
2020-03-03 08:17:19 -0500

    ip configed

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ def getRawValueByKey(): return server.mongo.getRawValueByKey(request) if request
 
 
 if __name__ == '__main__':
-    server.app.run(host='0.0.0.0', port=5000,debug=True)
+    server.app.run(host='127.0.0.1', port=5000,debug=True)
