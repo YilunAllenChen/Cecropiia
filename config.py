f76ab8160ee0695f6abc934eb5f5c4fd487d792e
YilunAllenChen <allenchen@gatech.edu>
2020-03-03 08:35:30 -0500

    changed mongoConfig

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-mongoConfig = "mongodb+srv://cecropia_admin:Slothbot@allenchen-h25gi.mongodb.net/test?retryWrites=true&w=majority"
+mongoConfig = "mongodb+srv://cecropia_general:CecropiiA@allenchen-h25gi.mongodb.net/test?retryWrites=true&w=majority"
 
